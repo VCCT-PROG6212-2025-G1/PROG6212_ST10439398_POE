b3c031966aad0886f833d3abc64f46ccb8e83894
ST10439398 - Morgan Gibbon <st10439398@vcconnect.edu.za>
2025-11-18 15:09:23 +0200

    rebuild project

--- CMCS.Tests/obj/Debug/net8.0/CMCS.Tests.AssemblyInfo.cs
+++ CMCS.Tests/obj/Debug/net8.0/CMCS.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3bcf891132ae4826a60ac67e2521b11fd683485")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0731c843f16478e36153b927dba895971cb6a8c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
