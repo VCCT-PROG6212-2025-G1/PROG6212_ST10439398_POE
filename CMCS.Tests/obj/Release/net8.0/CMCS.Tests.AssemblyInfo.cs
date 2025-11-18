a53c6078a75e0dc5da55070f19db00a1b2230003
ST10439398 - Morgan Gibbon <st10439398@vcconnect.edu.za>
2025-11-18 14:47:12 +0200

    update

--- CMCS.Tests/obj/Release/net8.0/CMCS.Tests.AssemblyInfo.cs
+++ CMCS.Tests/obj/Release/net8.0/CMCS.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d5e91e2b7b6631fac83df95340d2c4386b239f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cec923e958f2970064d70f3c683ef45ae0e81f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
