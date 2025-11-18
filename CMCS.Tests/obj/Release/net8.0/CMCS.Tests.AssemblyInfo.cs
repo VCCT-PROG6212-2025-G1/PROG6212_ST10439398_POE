1bd1811571df3d57057b161e17ab9189050d4ba8
ST10439398 - Morgan Gibbon <st10439398@vcconnect.edu.za>
2025-11-18 14:14:47 +0200

    Part 3 Initial Commit

--- CMCS.Tests/obj/Release/net8.0/CMCS.Tests.AssemblyInfo.cs
+++ CMCS.Tests/obj/Release/net8.0/CMCS.Tests.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS.Tests")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d5e91e2b7b6631fac83df95340d2c4386b239f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS.Tests")]
