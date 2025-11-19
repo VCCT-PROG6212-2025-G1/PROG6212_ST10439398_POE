20677950d753e92fa67997cf29d521cdf6152a55
ST10439398 - Morgan Gibbon <st10439398@vcconnect.edu.za>
2025-11-19 13:07:53 +0200

    update files

--- CMCS/obj/Debug/net8.0/CMCS.AssemblyInfo.cs
+++ CMCS/obj/Debug/net8.0/CMCS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+315323b2bc0e66af352e0e0a8249d84fe2319d52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fefbfe2dc22fff42d2fe4926ef32ea7e84220135")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
