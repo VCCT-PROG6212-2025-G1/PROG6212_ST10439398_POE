f6727cc0320a48c86751be0882950d57691f2816
ST10439398 - Morgan Gibbon <st10439398@vcconnect.edu.za>
2025-11-20 11:45:05 +0200

    HrController Created
    Create HRController for HR dashboard and user management

--- CMCS/obj/Debug/net8.0/CMCS.AssemblyInfo.cs
+++ CMCS/obj/Debug/net8.0/CMCS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ccc459ba1ef4cda79f341ea308cd4e44a2fc356")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3960020acaf9aa4ccb2944be3252287b782d5f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
